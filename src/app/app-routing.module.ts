8ac36e78f36c25806d7588544c5e222a63184e96
dinanProject <elnino.tube@gmail.com>
2026-07-25 16:41:36 +0700

    chore: upgrade Angular core and CLI to 15

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
 
 @NgModule({
 	imports: [
-		RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }),
+		RouterModule.forRoot(routes, {}),
 	],
 	exports: [RouterModule],
 })
