bbd537cef553abd45ca4a5a21c603ef513a944bd
dinanProject <elnino.tube@gmail.com>
2026-07-25 18:16:02 +0700

    chore: upgrade Angular to version 19

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,11 @@ import {
 import { Injectable, LOCALE_ID, NgModule } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
 import localeId from '@angular/common/locales/id';
-import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import {
+	HTTP_INTERCEPTORS,
+	provideHttpClient,
+	withInterceptorsFromDi,
+} from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import * as Hammer from 'hammerjs';
@@ -59,27 +63,27 @@ export class PatchedGestureConfig extends HammerGestureConfig {
 		},
 	};
 }
-
-@NgModule({ declarations: [AppComponent, UnauthorizedComponent],
-    bootstrap: [AppComponent], imports: [BrowserModule,
-        AppRoutingModule,
-        BrowserAnimationsModule,
-        HammerModule,
-        UiModule], providers: [
-        provideEnvironmentNgxCurrency(currencyConfig),
-        {
-            provide: HTTP_INTERCEPTORS,
-            useClass: AuthInterceptor,
-            multi: true,
-        },
-        {
-            provide: LOCALE_ID,
-            useValue: 'id',
-        },
-        {
-            provide: HAMMER_GESTURE_CONFIG,
-            useClass: PatchedGestureConfig,
-        },
-        provideHttpClient(withInterceptorsFromDi()),
-    ] })
+@NgModule({
+	declarations: [AppComponent, UnauthorizedComponent],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		HammerModule,
+		UiModule,
+	],
+	providers: [
+		provideEnvironmentNgxCurrency(currencyConfig),
+		{
+			provide: HTTP_INTERCEPTORS,
+			useClass: AuthInterceptor,
+			multi: true,
+		},
+		{
+			provide: LOCALE_ID,
+			useValue: 'id',
+		},
+	],
+	bootstrap: [AppComponent],
+})
 export class AppModule {}
--- src/app/shared/option-dialog/option-dialog.component.ts
+++ src/app/shared/option-dialog/option-dialog.component.ts
@@ -1,14 +1,24 @@
-import { Component, ContentChild, Directive, EventEmitter, forwardRef, Input, OnInit, Output, TemplateRef } from '@angular/core';
+import {
+	Component,
+	ContentChild,
+	Directive,
+	EventEmitter,
+	forwardRef,
+	Input,
+	Output,
+	TemplateRef,
+} from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { Observable } from 'rxjs';
 import { DialogComponent } from './dialog/dialog.component';
 
 @Directive({
-    selector: '[option]',
-    standalone: false
+	selector: '[option]',
+	standalone: false,
 })
-export class OptionDirective { }
+export class OptionDirective {}
+
 export interface OptionItem {
 	optionId: number;
 	optionName: string;
@@ -16,51 +26,60 @@ export interface OptionItem {
 
 export type OnOptionAdd = (optionName: string) => Promise<any>;
 export type OnOptionChanged<T> = (option: T) => Promise<T>;
-@Component({
-    selector: 'option-dialog',
-    templateUrl: './option-dialog.component.html',
-    styleUrls: ['./option-dialog.component.scss'],
-    providers: [
-        {
-            provide: NG_VALUE_ACCESSOR,
-            multi: true,
-            useExisting: forwardRef(() => OptionDialogComponent),
-        }
-    ],
-    host: {
-        '(change)': 'onChange()'
-    },
-    standalone: false
-})
-export class OptionDialogComponent<T> implements ControlValueAccessor {
 
-	@Input() title: string;
+@Component({
+	selector: 'option-dialog',
+	templateUrl: './option-dialog.component.html',
+	styleUrls: ['./option-dialog.component.scss'],
+	providers: [
+		{
+			provide: NG_VALUE_ACCESSOR,
+			multi: true,
+			useExisting: forwardRef(() => OptionDialogComponent),
+		},
+	],
+
+	standalone: false,
+})
+export class OptionDialogComponent<
+	T extends Record<string, unknown>,
+> implements ControlValueAccessor {
+	@Input() title!: string;
 	@Input() placeholder: string = '';
-	@Input() options: Observable<T[]>;
-	@Input() initialValue: T;
-	@Input() valueName: string = 'optionName';
+	@Input() options!: Observable<T[]>;
+
+	/**
+	 * Initial text displayed before an option object is selected.
+	 */
+	@Input() initialValue: string = '';
+
+	/**
+	 * Property name from the option object used as its display value.
+	 */
+	@Input() valueName: keyof T = 'optionName' as keyof T;
+
 	@Input() addOption: boolean = false;
 	@Input() isInvalid: boolean = false;
 	@Input() readonly: boolean = false;
 
-	@Output() add: EventEmitter<T> = this.readonly ? null : new EventEmitter<T>();
-	@Output() onSelect: EventEmitter<T> = this.readonly ? null : new EventEmitter<T>();
+	@Output() add: EventEmitter<T> = new EventEmitter<T>();
+	@Output() onSelect: EventEmitter<T> = new EventEmitter<T>();
 
 	@ContentChild(OptionDirective, { static: true, read: TemplateRef })
-	optionTemplate: TemplateRef<OptionDirective>;
-	option: T;
-	disabled: boolean = this.readonly;
+	optionTemplate!: TemplateRef<OptionDirective>;
+
+	option!: T;
+	disabled: boolean = false;
 	touched: boolean = false;
 
-	onTouched = (t: T) => { };
-	onChanged = (t: T) => { };
+	onTouched: () => void = () => {};
+	onChanged: (option: T) => void = () => {};
 
-	constructor(
-		private dialog: MatDialog
-	) { }
+	constructor(private dialog: MatDialog) {}
 
-	openDialog() {
+	openDialog(): void {
 		this.markAsTouched();
+
 		this.dialog
 			.open(DialogComponent, {
 				width: '400px',
@@ -69,43 +88,53 @@ export class OptionDialogComponent<T> implements ControlValueAccessor {
 					placeholder: this.placeholder,
 					options: this.options,
 					onAdd: this.addOption ? this.add : null,
-					optionTemplate: this.optionTemplate
-				}
+					optionTemplate: this.optionTemplate,
+				},
 			})
 			.afterClosed()
-			.subscribe((option: T) => {
-				if (option) {
-					this.writeValue(option);
-					this.onChanged(option);
-					this.onSelect.emit(option);
+			.subscribe((option: T | undefined) => {
+				if (!option) {
+					return;
 				}
-			})
+
+				this.writeValue(option);
+				this.onChanged(option);
+				this.onSelect.emit(option);
+			});
 	}
 
 	writeValue(option: T): void {
 		this.option = option;
 	}
 
-	registerOnChange(fn: any): void {
-		this.onChanged = fn; // <-- save the function
+	registerOnChange(fn: (option: T) => void): void {
+		this.onChanged = fn;
 	}
 
-	registerOnTouched(fn: any): void {
-		this.onTouched = fn; // <-- save the function
+	registerOnTouched(fn: () => void): void {
+		this.onTouched = fn;
 	}
 
-	setDisabledState(isDisabled: boolean) {
+	setDisabledState(isDisabled: boolean): void {
 		this.disabled = isDisabled;
 	}
 
-	markAsTouched() {
-		if (!this.touched) {
-			this.onTouched(this.option);
-			this.touched = true;
+	markAsTouched(): void {
+		if (this.touched) {
+			return;
 		}
+
+		this.onTouched();
+		this.touched = true;
 	}
 
 	get displayedValue(): string {
-		return this.option ? this.option[this.valueName] : this.initialValue;
+		if (!this.option) {
+			return this.initialValue;
+		}
+
+		const value = this.option[this.valueName];
+
+		return value == null ? '' : String(value);
 	}
 }
