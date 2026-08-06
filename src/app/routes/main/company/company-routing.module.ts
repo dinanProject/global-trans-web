3a02f17e55ccd0c93c0761794b5bba2d290fbfe9
dinanProject <elnino.tube@gmail.com>
2026-08-06 14:38:07 +0700

    protect application routes with permissions

--- src/app/routes/main/company/company-routing.module.ts
+++ src/app/routes/main/company/company-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { CompanyComponent } from './company.component';
+import { PermissionGuard } from 'src/app/core/guards/permission-guard';
 
 const routes: Routes = [
 	{
 		path: '',
 		component: CompanyComponent,
+		canActivate: [PermissionGuard],
+		data: {
+			permission: 'COMPANY.VIEW',
+		},
 	},
 ];
 
--- src/app/routes/main/menu-management/menu-management-routing.module.ts
+++ src/app/routes/main/menu-management/menu-management-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MenuManagementComponent } from './menu-management.component';
+import { PermissionGuard } from 'src/app/core/guards/permission-guard';
 
 const routes: Routes = [
 	{
 		path: '',
 		component: MenuManagementComponent,
+		canActivate: [PermissionGuard],
 		data: {
 			title: 'Administration',
 			subtitle: '',
+			permission: 'MENU.VIEW',
 		},
 	},
 ];
